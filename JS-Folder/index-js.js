e5797530a02b38bb45f72733562a7423337bba3d
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-02-02 18:25:26 +0100

    d

--- JS-Folder/index-js.js
+++ JS-Folder/index-js.js
@@ -3,6 +3,34 @@
 console.log("autor: florian");
 
 
-function calculateInsurance() {
-	
+
+var name = document.getElementById("wieso");
+var age = document.getElementById("alter").value;
+var horsepower = document.getElementById("pferde").value;
+var country = document.getElementById("land").value;
+var c = 12;
+
+function wert(){
+	document.getElementById("print").innerHTML = c;
 }
+
+
+
+
+// // function bald(){
+
+// // // if (country = Austria){
+// // // 	result = (Number(horsepower) * 100) / (Number(age) + 50); 
+// // // } else if (country = Hungary){
+// // // 	result = (Number(horsepower) * 120) / (Number(age) + 100);
+// // // } else (country = Greece){
+// // // 	result = (Number(horsepower) * 150) / (((Number)age+3) + 50);
+// // // }
+
+
+// document.getElementById("print").innerHTML = name + ", your insurance costs: " + result; 
+// }
+
+var elementNode = document.getElementById("button");
+elementNode.onclick = wert;
+
